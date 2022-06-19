ec51d0171f0a6fb9434efb93b16da0395898e629
cometbroom <alawi.wow@gmail.com>
2022-06-19 10:56:39 +0200

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
 import LandingContent from "../src/components/LandingContent";
-import Layout from "../src/components/layout";
 import Navbar from "../src/components/Navbar";
-import configurateTheme from "../src/tools/ThemeConfig";
-
 
 export default function Home() {
   return (
