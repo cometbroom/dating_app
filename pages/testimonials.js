b7761304f7c3913f407c57d6c47b9425c8c1de53
cometbroom <alawi.wow@gmail.com>
2022-06-19 10:55:08 +0200

    Error page added

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -1,6 +1,7 @@
 import { CircularProgress } from "@mui/material";
 import useFetch from "../src/hooks/useFetch";
 import Alert from "@mui/material/Alert";
+import { useRouter } from "next/router";
 
 import TestimonialView from "../src/views/TestimonialView";
 import Navbar from "../src/components/Navbar";
@@ -9,17 +10,22 @@ export default function Testimonials() {
   const [data, loading, error] = useFetch(
     "https://randomuser.me/api/?results=10"
   );
+  const router = useRouter();
+
+  const navigate = (path) => {
+    router.push(path);
+  };
 
   return (
     <>
+      <Alert severity="error">Could not fetch users: {error}</Alert>
+
       <Navbar></Navbar>
-      {error && (
-        <Alert severity="error">Could not fetch users: {error.message}</Alert>
-      )}
+      {error && navigate(`/error/${error}`)}
       {loading ? (
         <CircularProgress />
       ) : (
-        <TestimonialView users={data.results} />
+        data && <TestimonialView users={data.results} />
       )}
     </>
   );
