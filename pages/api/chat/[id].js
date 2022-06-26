55a76be54eb7412fa971925d152d9bc2f5f2abfb
cometbroom <alawi.wow@gmail.com>
2022-06-26 16:11:17 +0200

    Endpoint fix

--- pages/api/chat/[id].js
+++ pages/api/chat/[id].js
@@ -4,28 +4,27 @@ import { HttpResponder } from "../../../src/tools/HttpResponder";
 import { ObjectId } from "mongodb";
 import { unstable_getServerSession } from "next-auth/next";
 import { AUTH_OPTIONS } from "../auth/[...nextauth]";
+
 const handler = nextConnect();
 
 handler.use(middleware);
 
-handler.get(async (req, res) => {
+handler.post(async (req, res) => {
   try {
     const session = await unstable_getServerSession(req, res, AUTH_OPTIONS);
     if (!session)
       return HttpResponder.UNAUTHORIZED(res, { msg: "Unauthorized" });
     const coll = req.db.collection("users");
-    console.log(session.user.id);
-    const foundUser = await coll.findOne({
-      _id: new ObjectId(session.user.id),
-    });
-    if (!foundUser)
-      return HttpResponder.NOT_FOUND(res, { msg: "User not found" });
-    const peerId = await coll.findOne({ _id: foundUser.chats[0] });
-    return HttpResponder.OK(res, peerId.peerId);
+    const ack = await coll.findOneAndUpdate(
+      { _id: new ObjectId(session.user.id) },
+      { $set: { peerId: req.query.id } },
+      { upsert: true }
+    );
+
+    return HttpResponder.OK(res, { msg: "succeeded" });
   } catch (error) {
     HttpResponder.BAD_REQ(res, { msg: error.message });
   }
 });
-
 
 export default handler;
--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -8,16 +8,10 @@ const history = [
   { sender: "Boom", text: "Hey how you doing?", sent: false },
 ];
 
-function getRandomId() {
-  let min = Math.ceil(10000000);
-  let max = Math.floor(99999999);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 export default function ChatController() {
   const [session, loadSession, errSession] = useSession();
   const [connection, setConnection] = useState();
-  // const [peer] = usePeer(session);
+  const [peer] = usePeer(session);
 
   function sendMsg(text) {
     console.log(text, "will be sent");
