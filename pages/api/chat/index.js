734cfe853b58294e23be3932ca03c7043815f106
cometbroom <alawi.wow@gmail.com>
2022-06-26 16:07:18 +0200

    Facilitation endpoints setup

--- pages/api/chat/index.js
+++ pages/api/chat/index.js
@@ -3,20 +3,48 @@ import middleware from "../../../middleware/database";
 import { PEER_IDS } from "../../../src/tools/constants";
 import { HttpResponder } from "../../../src/tools/HttpResponder";
 import { v4 as uuidv4 } from "uuid";
-
+import { ObjectId } from "mongodb";
+import { unstable_getServerSession } from "next-auth/next";
+import { AUTH_OPTIONS } from "../auth/[...nextauth]";
 const handler = nextConnect();
 
 handler.use(middleware);
 
-const availableIds = [PEER_IDS.id1, PEER_IDS.id2];
-let index = 0;
-
 handler.get(async (req, res) => {
-  const fakeId = req.query.fakeId;
-  if (fakeId < 0 || fakeId >= availableIds.length)
-    return HttpResponder.BAD_REQ(res, { msg: "no id could be found" });
+  try {
+    const session = await unstable_getServerSession(req, res, AUTH_OPTIONS);
+    if (!session)
+      return HttpResponder.UNAUTHORIZED(res, { msg: "Unauthorized" });
+    const coll = req.db.collection("users");
+    console.log(session.user.id);
+    const foundUser = await coll.findOne({
+      _id: new ObjectId(session.user.id),
+    });
+    if (!foundUser)
+      return HttpResponder.NOT_FOUND(res, { msg: "User not found" });
+    const peerId = await coll.findOne({ _id: foundUser.chats[0] });
+    return HttpResponder.OK(res, peerId.peerId);
+  } catch (error) {
+    HttpResponder.BAD_REQ(res, { msg: error.message });
+  }
+});
 
-  return HttpResponder.OK(res, { id: uuidv4() });
+handler.post(async (req, res) => {
+  try {
+    const session = await unstable_getServerSession(req, res, AUTH_OPTIONS);
+    if (!session)
+      return HttpResponder.UNAUTHORIZED(res, { msg: "Unauthorized" });
+    const coll = req.db.collection("users");
+    const ack = await coll.findOneAndUpdate(
+      { _id: new ObjectId(session.user.id) },
+      { $set: { peerId: req.body.id } },
+      { upsert: true }
+    );
+
+    return HttpResponder.OK(res, { msg: "succeeded" });
+  } catch (error) {
+    HttpResponder.BAD_REQ(res, { msg: error.message });
+  }
 });
 
 export default handler;
