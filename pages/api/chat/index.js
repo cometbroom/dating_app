4446c9b6bc983cc062f99d2ef162f1a1c3c1da09
cometbroom <alawi.wow@gmail.com>
2022-06-28 18:26:25 +0200

    Cleaup

--- pages/api/chat/index.js
+++ pages/api/chat/index.js
@@ -1,7 +1,6 @@
 import nextConnect from "next-connect";
 import middleware from "../../../middleware/database";
 import { HttpResponder } from "../../../src/tools/HttpResponder";
-import { ObjectId } from "mongodb";
 import { unstable_getServerSession } from "next-auth/next";
 import { AUTH_OPTIONS } from "../auth/[...nextauth]";
 const handler = nextConnect();
@@ -10,7 +9,7 @@ const GET_CHATS_AGG = (array) => [
   {
     $match: {
       _id: {
-        $in: array.map((x) => new ObjectId(x.id)),
+        $in: array,
       },
     },
   },
@@ -35,7 +34,6 @@ handler.get(async (req, res) => {
     const peerId = await coll
       .aggregate(GET_CHATS_AGG(session.user.chats))
       .toArray();
-    console.log("fetched", peerId);
 
     return HttpResponder.OK(res, peerId);
   } catch (error) {
